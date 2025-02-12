da5d24f713f17ad8fea14691a74d83e8fc1d3d7a
Boaz <webevolution13@gmail.com>
2025-02-12 16:27:43 +0100

    upDate
    front end

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,39 @@
-<script setup lang="ts">
+<script setup>
 import { fetchPosts, truncateContent } from '~/utils/js/utils'
 
 const itemsPost = await fetchPosts()
 const latestPosts = itemsPost.value?.slice(-3) || []
+
+import { useNuxtApp } from '#app'
+const nuxtApp = useNuxtApp()
+
+const form = ref({
+  name: '',
+  email: '',
+  phonenumber: '',
+  message: '',
+})
+
+const isSent = ref(false)
+
+const sendEmail = async () => {
+  try {
+    await nuxtApp.$mail.send({
+      subject: `Új üzenetet küldött - ${form.value.name}`,
+      html: `
+        <p><strong>Name:</strong> ${form.value.name}</p>
+        <p><strong>Email:</strong> ${form.value.email}</p>
+        <p><strong>Phone Number:</strong> ${form.value.phonenumber}</p>
+        <p><strong>Message:</strong></p>
+        <p>${form.value.message}</p>
+      `,
+    })
+    isSent.value = true
+  } catch (error) {
+    console.error('Error sending email:', error)
+    alert('Failed to send email.')
+  }
+}
 </script>
 
 <template>
@@ -140,9 +171,10 @@ const latestPosts = itemsPost.value?.slice(-3) || []
       <p class="contact-form__description">
         Keressen minket még ma, és kérjen személyre szabott ajánlatot!
       </p>
-      <form class="contact-form__form">
+      <form @submit.prevent="sendEmail" class="contact-form__form">
         <div class="contact-form__input-group">
           <input
+            v-model="form.name"
             type="text"
             class="contact-form__input"
             placeholder="Név"
@@ -151,6 +183,7 @@ const latestPosts = itemsPost.value?.slice(-3) || []
         </div>
         <div class="contact-form__input-group">
           <input
+            v-model="form.email"
             type="email"
             class="contact-form__input"
             placeholder="Email cím"
@@ -159,6 +192,7 @@ const latestPosts = itemsPost.value?.slice(-3) || []
         </div>
         <div class="contact-form__input-group">
           <input
+            v-model="form.phonenumber"
             type="tel"
             class="contact-form__input"
             placeholder="Telefonszám"
@@ -167,6 +201,7 @@ const latestPosts = itemsPost.value?.slice(-3) || []
         </div>
         <div class="contact-form__input-group">
           <input
+            v-model="form.message"
             type="text"
             class="contact-form__input"
             placeholder="Takarítás típusa"
@@ -174,17 +209,24 @@ const latestPosts = itemsPost.value?.slice(-3) || []
           />
         </div>
         <div class="contact-form__input-group">
-          <p class="contact-form__input-group-description text-color-w">
-            A küldés gombra kattintva elfogadja az Adatvédelmi Tájékoztatót
-          </p>
-          <button type="submit" class="contact-form__button">
-            <span class="contact-form__button-text">Küldés</span>
-            <NuxtImg
-              class="contact-form__button-icon"
-              src="/img/blocks/mail.svg"
-              alt="NSZV takarítás"
-            />
-          </button>
+          <div v-if="!isSent" class="contact-form__input-group-checkbox">
+            <p class="contact-form__input-group-description text-color-w">
+              A küldés gombra kattintva elfogadja az Adatvédelmi Tájékoztatót
+            </p>
+            <button type="submit" class="contact-form__button">
+              <span class="contact-form__button-text">Küldés</span>
+              <NuxtImg
+                class="contact-form__button-icon"
+                src="/img/blocks/mail.svg"
+                alt="NSZV takarítás"
+              />
+            </button>
+          </div>
+          <div v-if="isSent" class="confirmation-message">
+            <p class="confirmation-message__p text-color text-center">
+              Köszönjük az üzenetét, hamarosan felvesszük Önnel a kapcsolatot.
+            </p>
+          </div>
         </div>
       </form>
     </div>
